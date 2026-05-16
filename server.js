df2460bd18181e00813c7429d3b892cf0345f61e
ZéyyTeams <zeyyteamsofficial@gmail.com>
2026-05-17 00:55:33 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/chat", async (req, res) => {
+
+  const userMessage = req.body.message;
+
+  const response = await fetch(
+    "https://api.groq.com/openai/v1/chat/completions",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": Bearer ${process.env.GROQ_API_KEY}
+      },
+      body: JSON.stringify({
+        model: "llama-3.3-70b-versatile",
+        messages: [
+          {
+            role: "user",
+            content: userMessage
+          }
+        ]
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  res.json(data);
+
+});
+
+app.listen(3000, () => {
+  console.log("server jalan");
+});
